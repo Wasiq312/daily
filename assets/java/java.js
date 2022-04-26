9e50c241d395216fff28fe152fa38f8e2963b415
Wasiq312 <95560466+Wasiq312@users.noreply.github.com>
2022-04-26 18:12:45 +0500

    DailyFitness

---
+++ assets/java/java.js
@@ -0,0 +1,30 @@
+var countDownDate = new Date("Jun 5, 2022 15:37:25").getTime();
+
+var x = setInterval(function() {
+
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (900 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h " +
+        minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+let menu = document.querySelector('#menu');
+
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
